fb9b21a9316e458fc6d7d1cee3ff55fffb92d82e
Naser Rafinia <Naser.r@outlook.com>
2020-05-09 16:36:20 +0430

    Fix bug

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using FASTER.core;
 using GrpcCache.Client;
+using Utf8Json;
 
 namespace Test
 {
@@ -12,6 +15,10 @@ namespace Test
     {
         public static void Main(string[] args)
         {
+            //Test2();
+            //Test3();
+            //return;
+
             var bDay = new DateTime(1984, 8, 28);
             var age = DateTime.Now.Year - bDay.Year;
             if (bDay > DateTime.Now.AddYears(-age))
@@ -23,6 +30,7 @@ namespace Test
             };
             var person = new Person()
             {
+                Id=100,
                 Firstname = "Naser",
                 Surname = "Rafinia",
                 Age = age,
@@ -39,11 +47,11 @@ namespace Test
             var p = cache.Get<Person>("TestModel");
             var items = cache.GetAll<Person>("");
             var exists = cache.Exists<Person>("TestModel");
-            
+
             cache.Remove<Person>("TestModel");
             items = cache.GetAll<Person>("");
             exists = cache.Exists<Person>("TestModel");
-            
+
             cache.RemoveAll<Person>();
             items = cache.GetAll<Person>("");
 
@@ -59,10 +67,149 @@ namespace Test
             Console.ReadKey();
         }
 
+        private static void Test2()
+        {
+            // This sample uses class key and value types, which are not blittable (i.e., they
+            // require a pointer to heap objects). Such datatypes include variable length types
+            // such as strings. You can override the default key equality comparer in two ways;
+            // (1) Make Key implement IFasterEqualityComparer<Key> interface
+            // (2) Provide IFasterEqualityComparer<Key> instance as param to constructor
+            // FASTER stores the actual objects in a separate object log.
+            // Note that serializers are required for class types, see below.
+
+            var dir = Path.GetTempPath()+"\\Faster";
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            var log = Devices.CreateLogDevice(Path.GetTempPath() + "hlog.log");
+            var objlog = Devices.CreateLogDevice(Path.GetTempPath() + "hlog.obj.log");
+            
+            var h = new FasterKV
+                <MyKey, MyValue, MyInput, MyOutput, MyContext, MyFunctions>
+                (1L << 20, new MyFunctions(),
+                new LogSettings { LogDevice = log, ObjectLogDevice = objlog, MemorySizeBits = 29,CopyReadsToTail = true},
+                new CheckpointSettings(){CheckpointDir = ""},
+                new SerializerSettings<MyKey, MyValue> { keySerializer = () => new MyKeySerializer(), valueSerializer = () => new MyValueSerializer() }
+                );
+            
+            var context = default(MyContext);
+
+            // Each thread calls StartSession to register itself with FASTER
+            //var s = h.NewSession("Test");
+            var g = Guid.Parse("6eb89566-8ef4-4280-8559-50be0cd3fbfe");
+            h.Recover(g);
+            var s = h.ResumeSession("Test", out var aa);
+            
+            MyKey key;
+
+            key = new MyKey { Key = 23.ToString() };
+            var inp1 = default(MyInput);
+            MyOutput g11 = new MyOutput();
+            var status1 = s.Read(ref key, ref inp1, ref g11, context, 0);
+
+
+            for (int i = 0; i < 20000; i++)
+            {
+                key = new MyKey { Key = i.ToString() };
+                var value = new MyValue
+                {
+                    Value = i,
+                    ExpireDate = DateTime.Now.AddSeconds(new Random().Next(60))
+                };
+                s.Upsert(ref key, ref value, context, 0);
+
+                // Each thread calls Refresh periodically for thread coordination
+                if (i % 1024 == 0) s.Refresh();
+            }
+
+            h.Log.Flush(true);
+            
+            //h.TakeFullCheckpoint(out var aa);
+            
+            key = new MyKey { Key = 23.ToString() };
+            var input = default(MyInput);
+            MyOutput g1 = new MyOutput();
+            var status = s.Read(ref key, ref input, ref g1, context, 0);
+
+            if (status == Status.OK && g1.Value.Value.ToString() == key.Key)
+                Console.WriteLine("Success!");
+            else
+                Console.WriteLine("Error!");
+
+            MyOutput g2 = new MyOutput();
+            key = new MyKey { Key = 46.ToString() };
+            status = s.Read(ref key, ref input, ref g2, context, 0);
+
+            if (status == Status.OK && g2.Value.Value.ToString() == key.Key)
+                Console.WriteLine("Success!");
+            else
+                Console.WriteLine("Error!");
+
+            var inp = new MyValue()
+            {
+                Value = 1000,
+                ExpireDate = DateTime.Now.AddMinutes(1)
+            };
+            status = s.Upsert(ref key, ref inp, context, 0);
+
+            var outp = new MyOutput();
+            status = s.Read(ref key, ref input, ref outp, context, 0);
+
+            // Delete key, read to verify deletion
+            var output = new MyOutput();
+            s.Delete(ref key, context, 0);
+            status = s.Read(ref key, ref input, ref output, context, 0);
+            if (status == Status.NOTFOUND)
+                Console.WriteLine("Success!");
+            else
+                Console.WriteLine("Error!");
+
+
+            // Each thread ends session when done
+
+            s.Dispose();
+
+            // Dispose FASTER instance and log
+            h.Dispose();
+            log.Close();
+            objlog.Close();
+
+            Console.WriteLine("Press <ENTER> to end");
+            Console.ReadLine();
+        }
+
+        private static void Test3()
+        {
+            var bDay = new DateTime(1984, 8, 28);
+            var age = DateTime.Now.Year - bDay.Year;
+            if (bDay > DateTime.Now.AddYears(-age))
+                age--;
+
+            var test = new P2()
+            {
+                Name = "Test"
+            };
+            var person = new Person()
+            {
+                Id=100,
+                Firstname = "Naser",
+                Surname = "Rafinia",
+                Age = age,
+                Test = test
+            };
+
+            var ser= JsonSerializer.Serialize(person);
+            var obj = JsonSerializer.Deserialize<Person>(ser);
+
+        }
     }
 
-    //[ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
-    public class Person
+    public class Base
+    {
+        public int Id { get; set; }
+    }
+
+    public class Person:Base
     {
         public string Firstname { get; set; }
         public string Surname { get; set; }
---
+++ src/Test/Types.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using FASTER.core;
+
+namespace Test
+{
+    public class MyKey : IFasterEqualityComparer<MyKey>
+    {
+        public string Key;
+
+        public long GetHashCode64(ref MyKey k)
+        {
+            //return Utility.GetHashCode(k.key);
+
+            return GetInt64HashCode(k.Key);
+        }
+
+        public bool Equals(ref MyKey key1, ref MyKey key2)
+        {
+            //return key1.key == key2.key;
+            return GetInt64HashCode(key1.Key) == GetInt64HashCode(key2.Key);
+        }
+
+        private static long GetInt64HashCode(string strText)
+        {
+            long hashCode = 0;
+            if (string.IsNullOrEmpty(strText))
+                return hashCode;
+
+            //Unicode Encode Covering all character set
+            var byteContents = Encoding.Unicode.GetBytes(strText);
+            System.Security.Cryptography.SHA256 hash =
+                new System.Security.Cryptography.SHA256CryptoServiceProvider();
+            var hashText = hash.ComputeHash(byteContents);
+            //32Byte hashText separate
+            //hashCodeStart = 0~7  8Byte
+            //hashCodeMedium = 8~23  8Byte
+            //hashCodeEnd = 24~31  8Byte
+            //and Fold
+            var hashCodeStart = BitConverter.ToInt64(hashText, 0);
+            var hashCodeMedium = BitConverter.ToInt64(hashText, 8);
+            var hashCodeEnd = BitConverter.ToInt64(hashText, 24);
+            hashCode = hashCodeStart ^ hashCodeMedium ^ hashCodeEnd;
+            return hashCode;
+        }
+    }
+
+    public class MyKeySerializer : BinaryObjectSerializer<MyKey>
+    {
+        public override void Serialize(ref MyKey key)
+        {
+            writer.Write(key.Key);
+        }
+
+        public override void Deserialize(ref MyKey key)
+        {
+            key.Key = reader.ReadString();
+        }
+    }
+
+
+    public class MyValue
+    {
+        public int Value;
+        public DateTime ExpireDate;
+        //public object Item { get; set; }
+    }
+
+    public class MyValueSerializer : BinaryObjectSerializer<MyValue>
+    {
+        public override void Serialize(ref MyValue value)
+        {
+            writer.Write(value.ExpireDate.Ticks);
+            writer.Write(value.Value);
+        }
+
+        public override void Deserialize(ref MyValue value)
+        {
+            value.ExpireDate = new DateTime(reader.ReadInt64());
+            value.Value = reader.ReadInt32();
+        }
+    }
+
+    public class MyInput
+    {
+        public int Value;
+        public DateTime ExpireDate;
+    }
+
+    public class MyOutput
+    {
+        public MyValue Value;
+    }
+
+    public class MyContext { }
+
+    public class MyFunctions : IFunctions<MyKey, MyValue, MyInput, MyOutput, MyContext>
+    {
+        private long _count = 0;
+        public long Count => _count;
+
+        public void InitialUpdater(ref MyKey key, ref MyInput input, ref MyValue value)
+        {
+            if (value == null)
+                value = new MyValue();
+            value.ExpireDate = input.ExpireDate;
+            value.Value = input.Value;
+        }
+
+        public void CopyUpdater(ref MyKey key, ref MyInput input, ref MyValue oldValue, ref MyValue newValue)
+        {
+            newValue = oldValue;
+        }
+
+        public bool InPlaceUpdater(ref MyKey key, ref MyInput input, ref MyValue value)
+        {
+            value.Value += input.Value;
+            return true;
+        }
+
+        public void SingleReader(ref MyKey key, ref MyInput input, ref MyValue value, ref MyOutput dst) => dst.Value = value;
+
+        public void SingleWriter(ref MyKey key, ref MyValue src, ref MyValue dst)
+        {
+            dst = src;
+            Interlocked.Increment(ref _count);
+        }
+
+        public void ConcurrentReader(ref MyKey key, ref MyInput input, ref MyValue value, ref MyOutput dst) => dst.Value = value;
+
+        public bool ConcurrentWriter(ref MyKey key, ref MyValue src, ref MyValue dst)
+        {
+            dst = src;
+
+            if (src != null)
+                return true;
+
+            Interlocked.Decrement(ref _count);
+            return true;
+
+        }
+
+        public void ReadCompletionCallback(ref MyKey key, ref MyInput input, ref MyOutput output, MyContext ctx, Status status) { }
+
+        public void UpsertCompletionCallback(ref MyKey key, ref MyValue value, MyContext ctx)
+        {
+            Interlocked.Increment(ref _count);
+        }
+
+        public void RMWCompletionCallback(ref MyKey key, ref MyInput input, MyContext ctx, Status status) { }
+
+        public void DeleteCompletionCallback(ref MyKey key, MyContext ctx)
+        {
+            Interlocked.Decrement(ref _count);
+        }
+
+        public void CheckpointCompletionCallback(string sessionId, CommitPoint commitPoint) { }
+
+    }
+
+}
